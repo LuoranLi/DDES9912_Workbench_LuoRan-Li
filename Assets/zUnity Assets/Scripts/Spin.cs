da1a6e7129b76d00831f2a979f9674940a2e9cd1
LuoranLi <loringli724@gmail.com>
2025-10-17 02:22:10 +1100

    r

---
+++ Assets/zUnity Assets/Scripts/Spin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniamtionControoler : MonoBehaviour
+{
+
+    public float speed = 1;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //  transform.Rotate(0, 0,speed * Time.deltaTime);
+          transform.Rotate(0, speed * Time.deltaTime, 0);
+        
+    }
+
+
+    public void BtnRotate(float speed1) 
+    {
+        speed = speed1;
+    }
+}
